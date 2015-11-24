c266b20b3fd0819d1200520351177ac8604eb032
jleewebdev <Josh>
2015-11-24 18:47:58 +0800

    Initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Paddle paddle;
+	Rigidbody2D rb2d;
+	private bool hasStarted = false;
+	
+	private Vector3 paddleToBallVector;
+
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted){
+			// Lock the ball relative to the paddle
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			
+			// Wait for a mouse click to launch the ball
+			if (Input.GetMouseButton(0)){
+				print ("Mouse clicked");
+				hasStarted = true;
+				rb2d.velocity = new Vector2(2f, 10f);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	float mousePosInBlocks;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	Vector3 paddlePos = new Vector3 (0.5f, this.transform.position.y, 0f);
+	mousePosInBlocks = (Input.mousePosition.x / Screen.width * 16 - 7);
+	print (mousePosInBlocks);
+	paddlePos.x = Mathf.Clamp(mousePosInBlocks, -8f, 7f);
+	this.transform.position = paddlePos;
+	}
+}
